73e6be8b7bb2b43e492079065022376682cd7074
Michael Christen <Orbiter@users.noreply.github.com>
2018-10-30 17:28:58 +0100

    odroid adjustments

--- bin/telemetry.py
+++ bin/telemetry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
+import sys, getopt, json, os, datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
-import os 
+from urllib.parse import urlparse, parse_qsl
 
 # tools to extract metrics
 def getHostname():
@@ -12,20 +12,31 @@ def getHostip():
     return os.popen("ifconfig | grep inet | awk '/broadcast/ {print $2}'").readline().strip()
 
 def getCPUtemperature():
-    res = os.popen('vcgencmd measure_temp').readline()
-    return float(res.replace("temp=","").replace("'C\n",""))
+    try:
+        res = os.popen('[ `which vcgencmd` ] && vcgencmd measure_temp').readline()
+        return float(res.replace("temp=","").replace("'C\n",""))
+    except Exception as e:
+        return 0.0
 
 def getRAMinfo():
     p = os.popen('free')
     p.readline()
-    return(p.readline().split()[1:])
+    a = p.readline().split()[1:]
+    # free is not available on mac
+    return a if len(a) >= 6 else [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 def getCPUuse():
-    return float(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip())
+    return float(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip().replace(",","."))
+
 def getCPUload():
     return float(os.popen("top -n1 | head -1 | awk -F 'load average' '{print $2}' | awk '{print $2}'").readline().replace(",","").strip())
+
 def getCPUfreq():
-    return float(os.popen("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq").readline().strip()) / 1000.0
+    try:
+        return float(os.popen("[ -f /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq ] && cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq").readline().strip()) / 1000.0
+    except Exception as e:
+        return 0.0
+    
 def getCPUcount():
     return int(os.popen("nproc").readline().strip())
 
@@ -45,14 +56,15 @@ def parseXB2GB(space):
     if (space.endswith("K")): return float(space[:-1]) / 1024.0 / 1024.0
     return 0.0
         
-def getMetricsJson():
+def getMetricsJson(pretty):
     cpufreq = getCPUfreq() # do this first to prevent that other tasks cause overclocking
     RAM_stats = getRAMinfo()
     DISK_stats = getDiskSpace()
     ram_total = float(RAM_stats[0])
     ram_used = float(RAM_stats[1])
     ram_available = float(RAM_stats[5])
-    return json.dumps({
+    j = {
+        "timestamp":datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"), # we are using the "date_hour_minute_second" or "strict_date_hour_minute_second" format of elasticsearch as fornat for the date: yyyy-MM-dd'T'HH:mm:ss.
         "host_name": getHostname(),
         "host_ip": getHostip(),
         "cpu_freq_mhz": cpufreq,
@@ -71,27 +83,49 @@ def getMetricsJson():
         "disk_used_gb": parseXB2GB(DISK_stats[1]),
         "disk_free_gb": parseXB2GB(DISK_stats[2]),
         "disk_percent": int(DISK_stats[3].replace("%",""))
-    }, sort_keys=True, indent=2)
+    }
+    if pretty:
+        return json.dumps(j, sort_keys=True, indent=2)
+    else:
+        return json.dumps(j, sort_keys=True)
         
 # the http server
 class Server(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        
-    def do_HEAD(self):
-        self._set_headers()
-        
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write(getMetricsJson().encode())
-                
+        print(self.path)
+        pr = urlparse(self.path)
+        path = pr.path
+        query = dict(parse_qsl(pr.query))
+        pretty = "pretty" in query
+        if path.endswith("/status.json"):
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(getMetricsJson(pretty).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+
 def run(port = 5055):
     httpd = HTTPServer(("", port), Server)
     print("Starting httpd on port %d..." % port)
     httpd.serve_forever()
 
-print("metrics   : " + getMetricsJson())
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"pPd",[])
+    except getopt.GetoptError:
+        print("telemetry.py -p -P -d")
+        sys.exit(2)
 
-run()
+    for opt, arg in opts:
+        if opt == '-p':
+            print(getMetricsJson(False))
+        elif opt == '-P':
+            print(getMetricsJson(True))
+        elif opt == '-d':
+            run()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
