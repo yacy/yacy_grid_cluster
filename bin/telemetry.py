c6f1ca55e49ec63a6cf1e7b356de78e5c6c8a26d
Michael Christen <mc@yacy.net>
2019-09-21 10:29:18 +0100

    node12->node17

--- bin/telemetry.py
+++ bin/telemetry.py
@@ -12,12 +12,12 @@
 # sudo systemctl enable telemetry.service
 
 # commands to do this in the YaCy Grid:
-# ./run_node00_node12.sh "sudo apt-get install -y python3 python3-pip net-tools sysstat git"
-# ./run_node00_node12.sh "sudo pip3 install flask flask-cors psutil"
-# ./run_node00_node12.sh "mkdir git && cd git && git clone https://github.com/yacy/yacy_grid_cluster.git"
-# ./run_node00_node12.sh "sudo cp git/yacy_grid_cluster/bin/telemetry.service /lib/systemd/system/"
-# ./run_node00_node12.sh "sudo systemctl daemon-reload"
-# ./run_node00_node12.sh "sudo systemctl enable telemetry.service"
+# ./run_node00_node17.sh "sudo apt-get install -y python3 python3-pip net-tools sysstat git"
+# ./run_node00_node17.sh "sudo pip3 install flask flask-cors psutil"
+# ./run_node00_node17.sh "mkdir git && cd git && git clone https://github.com/yacy/yacy_grid_cluster.git"
+# ./run_node00_node17.sh "sudo cp git/yacy_grid_cluster/bin/telemetry.service /lib/systemd/system/"
+# ./run_node00_node17.sh "sudo systemctl daemon-reload"
+# ./run_node00_node17.sh "sudo systemctl enable telemetry.service"
 # finally all nodes must be restarted with
 # ./restart_node00_node17.sh
 #
