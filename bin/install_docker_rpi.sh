ede519e4b6c026d991ae7d65472aacbb1b4555eb
Michael Peter Christen <mc@yacy.net>
2020-05-31 12:44:54 +0100

    updated docker installation script

--- bin/install_docker_rpi.sh
+++ bin/install_docker_rpi.sh
@@ -1,3 +1,19 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# clean up
+sudo apt-get update
+suso apt-get -y upgrade
+sudo apt -y remove aufs-dkms
+sudo apt -y autoremove
+
+# disable swap
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+
+# install docker
+curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
+newgrp docker
+
+# also install docker-compose
+sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip
+sudo pip3 install docker-compose
